fd86e5e2589c44f84e35f4d04f0e9ceee356a747
Somil Dogra <151395938+somil-18@users.noreply.github.com>
2025-08-28 18:38:56 +0530

    Update models.py

--- Backend/models.py
+++ Backend/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.mysql import JSON
 
 db = SQLAlchemy()
 
@@ -10,12 +11,24 @@ class User(db.Model):
     is_verified = db.Column(db.Boolean, default=False)
     role = db.Column(db.String(20), default="user") 
 
+
 class Listing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     description = db.Column(db.Text, nullable=True)
+    amenities = db.Column(JSON)
     price = db.Column(db.Float, nullable=False)
     location = db.Column(db.String(200), nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     owner = db.relationship("User", backref=db.backref("listings", lazy=True))
+
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)   
+    listing_id = db.Column(db.Integer, db.ForeignKey("listing.id"), nullable=False)  
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    status = db.Column(db.String(20), default="Pending")  # Pending, Confirmed, Cancelled
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
