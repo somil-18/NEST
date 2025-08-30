5f2663961fea142aca30b64a4eb52d04cbb5a88f
Somil Dogra <151395938+somil-18@users.noreply.github.com>
2025-08-30 14:22:35 +0530

    Add files via upload

---
+++ Backend/run.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from app import create_app, db
+
+
+# load environment variables from .env file
+load_dotenv()
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
