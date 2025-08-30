1497635c688bdbce36ca72baaf05a0e9f94718cf
Somil Dogra <151395938+somil-18@users.noreply.github.com>
2025-08-30 16:57:15 +0530

    Update __init__.py

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from config import Config
 from .extensions import db, mail, jwt, cors
+import cloudinary
+import os
 
 
 def create_app(config_class=Config):
@@ -13,6 +15,13 @@ def create_app(config_class=Config):
     jwt.init_app(app)
     cors.init_app(app, resources={r"/*": {"origins": app.config['FRONTEND_URL']}}, supports_credentials=True)
 
+    cloudinary.config(
+        cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
+        api_key = os.getenv('CLOUDINARY_API_KEY'),
+        api_secret = os.getenv('CLOUDINARY_API_SECRET'),
+        secure=True # Ensures URLs are HTTPS
+    )
+
     # import and register blueprints
     from .routes.auth import auth_bp
     from .routes.listings import listings_bp
@@ -21,5 +30,6 @@ def create_app(config_class=Config):
     app.register_blueprint(auth_bp)
     app.register_blueprint(listings_bp)
     app.register_blueprint(bookings_bp)
+
 
     return app
