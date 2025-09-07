df6ef5a2e30fce4bd67ade4db3778295f8360711
Somil Dogra <151395938+somil-18@users.noreply.github.com>
2025-09-07 19:55:26 +0530

    Update run.py

--- Backend/run.py
+++ Backend/run.py
@@ -1,6 +1,4 @@
 from dotenv import load_dotenv
-from flask_cors import CORS
-import os
 
 
 load_dotenv()
@@ -10,11 +8,11 @@ from app import create_app, db
 
 
 app = create_app()
-CORS(app)
 
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
 
